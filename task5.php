07ace9290f59a6f9ff5538809772820986e36a30
Dan <danteast@inbox.ru>
2020-09-22 16:48:54 +0300

    added HW-1

---
+++ task5.php
@@ -0,0 +1,17 @@
+<?php
+echo "<br><p>Задание 5</p><br>";
+class A {
+    public function foo() {
+        static $x = 0;
+        echo ++$x;
+    }
+}
+$a1 = new A();
+$a2 = new A();
+$a1->foo(); //1
+$a2->foo(); //2
+$a1->foo(); //3
+$a2->foo(); //4
+
+//Переменная $x статична,поэтому сохраняет значения
+
